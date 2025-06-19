4b179e42bf4fd3d0f71c43d56c95354edc023edd
ichingxs4all <mragutlich@gmail.com>
2025-06-19 20:03:14 +0200

    Working setup

---
+++ Arduino/TFB-Arduino-19062025/pozyx.ino
@@ -0,0 +1,117 @@
+// error printing function for debugging
+void printErrorCode(String operation)
+{
+  uint8_t error_code;
+  if (remote_id == NULL)
+  {
+    Pozyx.getErrorCode(&error_code);
+    Serial.print("ERROR ");
+    Serial.print(operation);
+    Serial.print(", local error code: 0x");
+    Serial.println(error_code, HEX);
+    return;
+  }
+  int status = Pozyx.getErrorCode(&error_code, remote_id);
+  if (status == POZYX_SUCCESS)
+  {
+    Serial.print("ERROR ");
+    Serial.print(operation);
+    Serial.print(" on ID 0x");
+    Serial.print(remote_id, HEX);
+    Serial.print(", error code: 0x");
+    Serial.println(error_code, HEX);
+  }
+  else
+  {
+    Pozyx.getErrorCode(&error_code);
+    Serial.print("ERROR ");
+    Serial.print(operation);
+    Serial.print(", couldn't retrieve remote error code, local error: 0x");
+    Serial.println(error_code, HEX);
+  }
+}
+
+// print out the anchor coordinates (also required for the processing sketch)
+void printCalibrationResult()
+{
+  uint8_t list_size;
+  int status;
+
+  status = Pozyx.getDeviceListSize(&list_size, remote_id);
+  Serial.print("list size: ");
+  Serial.println(status * list_size);
+
+  if (list_size == 0)
+  {
+    printErrorCode("configuration");
+    return;
+  }
+
+  uint16_t device_ids[list_size];
+  status &= Pozyx.getDeviceIds(device_ids, list_size, remote_id);
+
+  Serial.println(F("Calibration result:"));
+  Serial.print(F("Anchors found: "));
+  Serial.println(list_size);
+
+  coordinates_t anchor_coor;
+  for (int i = 0; i < list_size; i++)
+  {
+    Serial.print("ANCHOR,");
+    Serial.print("0x");
+    Serial.print(device_ids[i], HEX);
+    Serial.print(",");
+    Pozyx.getDeviceCoordinates(device_ids[i], &anchor_coor, remote_id);
+    Serial.print(anchor_coor.x);
+    Serial.print(",");
+    Serial.print(anchor_coor.y);
+    Serial.print(",");
+    Serial.println(anchor_coor.z);
+  }
+}
+
+// function to manually set the anchor coordinates
+void setAnchorsManual()
+{
+  for (int i = 0; i < num_anchors; i++)
+  {
+    device_coordinates_t anchor;
+    anchor.network_id = anchors[i];
+    anchor.flag = 0x1;
+    anchor.pos.x = anchors_x[i];
+    anchor.pos.y = anchors_y[i];
+    anchor.pos.z = heights[i];
+    Pozyx.addDevice(anchor, remote_id);
+  }
+  if (num_anchors > 4)
+  {
+    Pozyx.setSelectionOfAnchors(POZYX_ANCHOR_SEL_AUTO, num_anchors, remote_id);
+  }
+}
+
+// prints the coordinates for either humans or for processing
+void printCoordinates(coordinates_t coor){
+  uint16_t network_id = remote_id;
+  if (network_id == NULL){
+    network_id = 0;
+  }
+  if(!use_processing){
+    Serial.print("POS ID 0x");
+    Serial.print(network_id, HEX);
+    Serial.print(", x(mm): ");
+    Serial.print(coor.x);
+    Serial.print(", y(mm): ");
+    Serial.print(coor.y);
+    Serial.print(", z(mm): ");
+    Serial.println(coor.z);
+  }else{
+    Serial.print("POS,0x");
+    Serial.print(network_id,HEX);
+    Serial.print(",");
+    Serial.print(coor.x);
+    Serial.print(",");
+    Serial.print(coor.y);
+    Serial.print(",");
+    Serial.println(coor.z);
+  }
+}
